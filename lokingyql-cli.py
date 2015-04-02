92c06e6a2f8cfd0f2e87f1155db9aff4c930d662
josuebrunel <josuebrunel@gmail.com>
2015-04-02 17:58:42 +0200

    just setting stuff up

--- lokingyql-cli.py
+++ lokingyql-cli.py
@@ -4,7 +4,7 @@
 #   Filename        : yql-cli.py
 #   Description     :
 #   Creation Date   : 02-04-2015
-#   Last Modified   : Thu 02 Apr 2015 05:20:57 PM CEST
+#   Last Modified   : Thu 02 Apr 2015 05:57:27 PM CEST
 #
 ##################################################
 
@@ -13,6 +13,7 @@ __email__   = 'josuebrunel@gmail.com'
 
 import pdb
 import sys
+import cmd
 import argparse
 
 from lokingyql import LokingYQL
@@ -32,6 +33,15 @@ class ExecuteAction(argparse.Action):
         print(response.json())
         sys.exit(0)
 
+class ShellAction(argparse.Action):
+
+    def __call__(self, parser, namespace, value, option_string=None):
+        pass
+
+class TableAction(argparse.Action):
+
+    def __call__(self, parser, namespace, value, option_string=None):
+        pass
 
 if __name__ == '__main__':
 
@@ -42,6 +52,18 @@ if __name__ == '__main__':
         '--command',
         action=ExecuteAction,
         help="Execute an YQL query"
+    )
+
+    parser.add_argument(
+        'shell',
+        action=ShellAction,
+        help="SQL like shell"
+    )
+
+    parser.add_argument(
+        'create-table',
+        action=TableAction,
+        help="Create a YQL Table from python file"
     )
 
     args = vars(parser.parse_args())
