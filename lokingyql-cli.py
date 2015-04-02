ba1f70a2f6a85b9637fa4e5d10482791b5ee8767
josuebrunel <josuebrunel@gmail.com>
2015-04-02 17:10:29 +0200

    #2: query are ok. Need to filter responses now

--- lokingyql-cli.py
+++ lokingyql-cli.py
@@ -4,17 +4,30 @@
 #   Filename        : yql-cli.py
 #   Description     :
 #   Creation Date   : 02-04-2015
-#   Last Modified   : Thu 02 Apr 2015 05:03:00 PM CEST
+#   Last Modified   : Thu 02 Apr 2015 05:08:23 PM CEST
 #
 ##################################################
 
 import pdb
+import sys
 import argparse
+
+from lokingyql import LokingYQL
 
 class ExecuteAction(argparse.Action):
 
     def __call__(self, parser, namespace, value, option_string=None):
-        pass        
+        
+        yql = LokingYQL(community=True)
+
+        response = yql.rawQuery(value)
+
+        if not response.status_code == 200:
+            print(response.content)
+            sys.exit(1)
+
+        print(response.json())
+        sys.exit(0)
 
 
 if __name__ == '__main__':
