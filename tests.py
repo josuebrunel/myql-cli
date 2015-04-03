1896a80e399f1be58661d0081fbcd09b70dc393b
josuebrunel <josuebrunel@gmail.com>
2015-04-03 15:41:57 +0000

    #1 : --jsonCompact tests added

---
+++ tests.py
@@ -0,0 +1,25 @@
+##################################################
+#
+#   Author          : josuebrunel
+#   Filename        : tests.py
+#   Description     :
+#   Creation Date   : 03-04-2015
+#   Last Modified   : Fri 03 Apr 2015 03:40:37 PM UTC
+#
+##################################################
+
+import os
+import unittest
+import subprocess
+
+class TestYqlShell(unittest.TestCase):
+
+    def execute(self, args):
+        query="'select * from geo.countries where name=\"Congo\"'"
+        cmd="python lokingyql-cli.py execute {0} {1}".format(args, query)
+        print(cmd)
+        exit_code = subprocess.call("{0}".format(cmd), shell=True)
+        return exit_code
+
+    def testJsonCompact(self,):
+        self.assertEquals(self.execute('--format json --jsonCompact'),0)
