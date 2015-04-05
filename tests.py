45c38b6d5731233ba6ef5c163853e16bb3b0f2a2
josuebrunel <josuebrunel@gmail.com>
2015-04-05 13:10:46 +0200

    logger name added

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@
 #   Filename        : tests.py
 #   Description     :
 #   Creation Date   : 03-04-2015
-#   Last Modified   : Sun 05 Apr 2015 01:00:07 PM CEST
+#   Last Modified   : Sun 05 Apr 2015 01:10:28 PM CEST
 #
 ##################################################
 
@@ -12,14 +12,18 @@ import os, logging
 import unittest
 import subprocess
 
-logging.basicConfig(level=logging.DEBUG,format="[%(asctime)s %(levelname)s] [%(funcName)s] %(message)s \n")
 
 class TestYqlQuery(unittest.TestCase):
+    
+    def setUp(self,):
+
+        logging.basicConfig(level=logging.DEBUG,format="[%(asctime)s %(levelname)s] [%(funcName)s] %(message)s \n")
+        self.logger = logging.getLogger(__name__)
 
     def execute(self, args):
         query="'select * from geo.countries where name=\"Congo\"'"
         cmd="python lokingyql-cli.py execute {0} {1}".format(args, query)
-        logging.debug(cmd)
+        self.logger.debug(cmd)
         print("\n")
         exit_code = subprocess.call("{0}".format(cmd), shell=True)
         return exit_code
